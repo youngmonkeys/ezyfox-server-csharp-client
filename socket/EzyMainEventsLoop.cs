5f96e6dd3663d39711f41f252149e722006ad8f6
Dung Ta Van <itprono3@gmail.com>
2019-08-04 14:22:56 +0700

    add main events loop

---
+++ socket/EzyMainEventsLoop.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace com.tvd12.ezyfoxserver.client.socket
+{
+    public class EzyMainEventsLoop
+    {
+        protected volatile bool active;
+        protected readonly EzyClients clients;
+        protected readonly IList<EzyClient> cachedClients;
+
+        public EzyMainEventsLoop() {
+            this.clients = EzyClients.getInstance();
+            this.cachedClients = new List<EzyClient>();
+        }
+
+        public void start()
+        {
+            start(3);
+        }
+
+        public void start(int sleepTime) 
+        {
+            this.active = true;
+            while (active)
+            {
+                Thread.Sleep(sleepTime);
+                clients.getClients(cachedClients);
+                foreach (EzyClient one in cachedClients)
+                    one.processEvents();
+            }
+        }
+
+        public void stop() 
+        {
+            this.active = false;
+        }
+    }
+}
