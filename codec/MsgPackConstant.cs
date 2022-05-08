69d66dc99306bf175a1506c24220f34cf35f87da
Dung Ta Van <itprono3@gmail.com>
2022-05-08 21:25:27 +0700

    update MsgPackConstant

--- codec/MsgPackConstant.cs
+++ codec/MsgPackConstant.cs
@@ -10,9 +10,9 @@ namespace com.tvd12.ezyfoxserver.client.codec
         public static readonly long MAX_UINT32 = EzyMath.bin2long(32);
 
 		public static readonly long MIN_NEGATIVE_FIXINT = -EzyMath.bin2int(5) - 1;
-		public static readonly long MIN_INT8 = -EzyMath.bin2int(7);
-		public static readonly long MIN_INT16 = -EzyMath.bin2int(15);
-		public static readonly long MIN_INT32 = -EzyMath.bin2int(31);
+		public static readonly long MIN_INT8 = -EzyMath.bin2int(7) - 1;
+		public static readonly long MIN_INT16 = -EzyMath.bin2int(15) - 1;
+		public static readonly long MIN_INT32 = -EzyMath.bin2int(31) - 1;
 
 		public static readonly int MAX_FIXMAP_SIZE = EzyMath.bin2int(4);
 		public static readonly int MAX_FIXARRAY_SIZE = EzyMath.bin2int(4);
