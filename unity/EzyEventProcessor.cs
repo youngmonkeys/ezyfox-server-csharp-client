8ef31c6cb0e6e8f2560cfc062a2888b4de6ae492
vu-luong <vubinhcht@gmail.com>
2024-01-04 22:28:19 +0700

    inherit new classes

--- unity/EzyEventProcessor.cs
+++ unity/EzyEventProcessor.cs
@@ -4,36 +4,14 @@ using UnityEngine;
 namespace com.tvd12.ezyfoxserver.client.unity
 {
 	[Obsolete("Please use EzyAbstractEventProcessor instead.")]
-	public abstract class EzyEventProcessor : MonoBehaviour
+	public abstract class EzyEventProcessor : EzyAbstractEventProcessor
 	{
-		private static EzyEventProcessor INSTANCE;
-		
 		[SerializeField]
 		private EzySocketConfigHolderVariable socketConfig;
 
-		private void Awake()
+		protected override string GetZoneName()
 		{
-			// If go back to current scene, don't make duplication
-			if (INSTANCE != null)
-			{
-				Destroy(gameObject);
-			}
-			else
-			{
-				INSTANCE = this;
-				DontDestroyOnLoad(gameObject);
-			}
-		}
-
-		void Update()
-		{
-			// Main thread pulls data from socket
-#if UNITY_WEBGL && !UNITY_EDITOR
-#else
-			EzyClients.getInstance()
-				.getClient(socketConfig.Value.Value.ZoneName)
-				.processEvents();
-#endif
+			return socketConfig.Value.Value.ZoneName;
 		}
 	}
 }
--- unity/EzySocketConfig.cs
+++ unity/EzySocketConfig.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace com.tvd12.ezyfoxserver.client.unity
 {
     [Serializable]
-    public struct EzySocketConfig
+    public class EzySocketConfig
     {
         [SerializeField] private string zoneName;
 
@@ -19,6 +19,19 @@ namespace com.tvd12.ezyfoxserver.client.unity
         [SerializeField] private bool udpUsage;
 
         [SerializeField] private bool enableSSL;
+
+        public EzySocketConfig(Builder builder)
+        {
+            zoneName = builder.GetZoneName();
+            appName = builder.GetAppName();
+            webSocketUrl = builder.GetWebSocketUrl();
+            tcpUrl = builder.GetTcpUrl();
+            udpPort = builder.GetUdpPort();
+            udpUsage = builder.IsUdpUsage();
+            enableSSL = builder.IsEnableSSL();
+        }
+
+        private EzySocketConfig() {}
 
         public string ZoneName
         {
@@ -60,6 +73,104 @@ namespace com.tvd12.ezyfoxserver.client.unity
         {
             get => enableSSL;
             set => enableSSL = value;
+        }
+
+        public static Builder GetBuilder()
+        {
+            return new Builder();
+        }
+
+        public class Builder
+        {
+            private string zoneName;
+            private string appName;
+            private string webSocketUrl;
+            private string tcpUrl;
+            private int udpPort;
+            private bool udpUsage;
+            private bool enableSSL;
+
+            public string GetZoneName()
+            {
+                return zoneName;
+            }
+            
+            public string GetAppName()
+            {
+                return appName;
+            }
+
+            public string GetWebSocketUrl()
+            {
+                return webSocketUrl;
+            }
+
+            public string GetTcpUrl()
+            {
+                return tcpUrl;
+            }
+
+            public int GetUdpPort()
+            {
+                return udpPort;
+            }
+
+            public bool IsUdpUsage()
+            {
+                return udpUsage;
+            }
+
+            public bool IsEnableSSL()
+            {
+                return enableSSL;
+            }
+
+            public Builder ZoneName(string zoneName)
+            {
+                this.zoneName = zoneName;
+                return this;
+            }
+
+            public Builder AppName(string appName)
+            {
+                this.appName = appName;
+                return this;
+            }
+
+            public Builder WebSocketUrl(string webSocketUrl)
+            {
+                this.webSocketUrl = webSocketUrl;
+                return this;
+            }
+
+            public Builder TcpUrl(string tcpUrl)
+            {
+                this.tcpUrl = tcpUrl;
+                return this;
+            }
+
+            public Builder UdpPort(int udpPort)
+            {
+                this.udpPort = udpPort;
+                return this;
+            }
+
+            public Builder UdpUsage(bool udpUsage)
+            {
+                this.udpUsage = udpUsage;
+                return this;
+            }
+
+            public Builder EnableSSL(bool enableSSL)
+            {
+                this.enableSSL = enableSSL;
+                return this;
+            }
+
+            public EzySocketConfig Build()
+            {
+                return new EzySocketConfig(this);
+            }
         }
     }
 }
