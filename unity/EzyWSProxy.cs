8e9096583a2b13241d3cd6a75f2942efdde3bdc5
vu-luong <vubinhcht@gmail.com>
2024-10-28 22:43:09 +1000

    update EzyWSProxy

--- unity/EzyWSProxy.cs
+++ unity/EzyWSProxy.cs
@@ -5,6 +5,7 @@ namespace com.tvd12.ezyfoxserver.client.unity
 {
 	public class EzyWSProxy
 	{
+#if UNITY_WEBGL && !UNITY_EDITOR
 		[DllImport("__Internal")]
 		public static extern void setEventHandlerCallback(
 			EzyDelegates.EventHandlerDelegate callback
@@ -35,5 +36,33 @@ namespace com.tvd12.ezyfoxserver.client.unity
 			String jsonData,
 			EzyDelegates.Delegate2 callback
 		);
+#else
+		public static void setEventHandlerCallback(
+			EzyDelegates.EventHandlerDelegate callback
+		) {}
+
+		public static void setDataHandlerCallback(
+			EzyDelegates.DataHandlerDelegate callback
+		) {}
+
+		public static void setDebug(bool debug) {}
+
+		public static bool isMobile() {
+			return false;
+		}
+
+		public static void run3(
+			String clientName,
+			String functionName,
+			EzyDelegates.Delegate2 callback
+		) {}
+
+		public static void run4(
+			String clientName,
+			String functionName,
+			String jsonData,
+			EzyDelegates.Delegate2 callback
+		) {}
+#endif
 	}
 }
