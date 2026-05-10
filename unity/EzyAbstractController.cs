afa803b84c138a9d2d10677b4b7f878981d3df84
vu-luong <vubinhcht@gmail.com>
2026-05-10 22:44:06 +1000

    expose unity reconnect config and raw connection failure callback

--- unity/EzyAbstractController.cs
+++ unity/EzyAbstractController.cs
@@ -48,11 +48,17 @@ namespace com.tvd12.ezyfoxserver.client.unity
 			);
 			if (socketProxy.getClient() == null)
 			{
-				var config = EzyClientConfig.builder()
+				var configBuilder = EzyClientConfig.builder()
 					.clientName(socketConfig.ZoneName)
 					.zoneName(socketConfig.ZoneName)
-					.enableSSL(socketConfig.EnableSSL)
-					.build();
+					.enableSSL(socketConfig.EnableSSL);
+				configBuilder
+					.reconnectConfigBuilder()
+					.enable(socketConfig.EnableReconnect)
+					.maxReconnectCount(socketConfig.MaxReconnectCount)
+					.reconnectPeriod(socketConfig.ReconnectPeriod)
+					.done();
+				var config = configBuilder.build();
 				EzyClientFactory
 					.getInstance()
 					.getOrCreateClient(
@@ -100,6 +106,13 @@ namespace com.tvd12.ezyfoxserver.client.unity
 		{
 			socketHandlers.Add(
 				socketProxy.onConnectionFailed(handler)
+			);
+		}
+
+		protected void OnRawConnectionFailed(EzySocketProxyEventHandler<EzyConnectionFailureEvent> handler)
+		{
+			socketHandlers.Add(
+				socketProxy.onRawConnectionFailed(handler)
 			);
 		}
 
